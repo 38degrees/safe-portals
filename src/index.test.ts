be1085ca1c37e017a1288d100a6d3af33f3143a9
Tom Morton <tom.m@38degrees.org.uk>
2020-03-03 12:18:07 +0000

    Initial commit

---
+++ src/index.test.ts
@@ -0,0 +1,70 @@
+import * as Safe from './index';
+
+describe('Type-safe composable serializers', () => {
+  test('object', () => {
+    const s = Safe.obj({
+      x: Safe.str,
+      y: Safe.array(Safe.int)
+    });
+    const o = { x: 'hi', y: [1,2,3] };
+
+    expect(s.read(s.write(o))).toEqual(o);
+    expect(() => s.read([])).toThrowError(Safe.ParseError);
+  });
+
+  test('nothing', () => {
+    expect(Safe.nothing.read(Safe.nothing.write())).toEqual(undefined);
+  });
+
+  test('nullable', () => {
+    const s = Safe.nullable(Safe.str);
+    expect(s.read(s.write(null))).toEqual(null);
+    expect(s.read(s.write('hi'))).toEqual('hi');
+  });
+
+  test('array', () => {
+    const s = Safe.array(Safe.int);
+    expect(s.read(s.write([1,2,3,4]))).toEqual([1,2,3,4]);
+    expect(() => s.read({})).toThrowError(Safe.ParseError);
+  });
+
+  test('boolean', () => {
+    expect(Safe.bool.read(Safe.bool.write(true))).toBeTruthy();
+    expect(Safe.bool.read(Safe.bool.write(false))).toBeFalsy();
+    expect(() => Safe.bool.read('true')).toThrowError(Safe.ParseError);
+  });
+
+  test('tuple', () => {
+    const s = Safe.tuple(Safe.str, Safe.optional(Safe.array(Safe.int)));
+    expect(s.read(s.write(["hi", undefined]))).toEqual(["hi", undefined]);
+    expect(s.read(s.write(["hi", [2,3]]))).toEqual(["hi", [2,3]]);
+    expect(() => s.read({})).toThrowError(Safe.ParseError);
+  });
+
+  test('date', () => {
+    const d = new Date();
+    expect(Safe.date.read(Safe.date.write(d))).toEqual(d);
+    expect(() => Safe.date.read('blah')).toThrowError(Safe.ParseError);
+    expect(() => Safe.date.read(null)).toThrowError(Safe.ParseError);
+  });
+
+  test('raw', () => {
+    const d = {'some': ['random', {stuff: 123}]};
+    expect(Safe.raw.read(Safe.raw.write(d))).toEqual(d);
+  });
+
+  test('str', () => {
+    expect(Safe.str.read(Safe.str.write("hi"))).toEqual("hi");
+    expect(() => Safe.str.read(null)).toThrowError(Safe.ParseError);
+  });
+
+  test('int', () => {
+    expect(Safe.int.read(Safe.int.write(123))).toEqual(123);
+    expect(() => Safe.int.read("erm")).toThrowError(Safe.ParseError);
+  });
+
+  test('float', () => {
+    expect(Safe.float.read(Safe.float.write(123.45))).toEqual(123.45);
+    expect(() => Safe.float.read("erm")).toThrowError(Safe.ParseError);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,159 @@
+/**
+ * Type-safe, composable serialization/unserialization.
+ *
+ * Safe-portals can be used wherever data traverses an un-typed boundary
+ * (eg from DB, tasks sent to resque, HTTP calls, routing information in
+ * URLs), in order to maintain static analysis across the un-typed boundary.
+ */
+export class ParseError extends Error {
+  constructor(wanted: string, got: any) {
+    super(`Parse error: expected ${wanted} but found '${got}'`);
+    // needed because JS is silly
+    // @ts-ignore
+    Object.setPrototypeOf(this, ParseError.prototype);
+  }
+}
+// why this wrapper with 'never' type? so we can put error handling into expressions
+const parseError = (wanted: string, got: any): never => {
+  throw new ParseError(wanted, got)
+}
+
+type UntypedJson = string | number | boolean | null | UntypedJson[] | { [key: string]: UntypedJson };
+type Reader<T> = (o: UntypedJson) => T;
+type Writer<T> = (t: T) => UntypedJson;
+interface SafeSerializer<T> { read: Reader<T>; write: Writer<T>; }
+
+export interface Tuple<T> extends SafeSerializer<T> { container: 'tuple' }
+export interface Value<T> extends SafeSerializer<T> { container: 'none' }
+export interface List<T> extends SafeSerializer<T> { container: 'list' }
+export interface Obj<T> extends SafeSerializer<T> { container: 'obj' }
+export type Type<T> = Tuple<T> | List<T> | Value<T> | Obj<T>;
+
+/**
+ * useful for getting the TS type that a safe serializer operates on.
+ * ie. TypeEncapsulatedBy<Type<T>> = T
+ */
+export type TypeEncapsulatedBy<T extends SafeSerializer<any>> = ReturnType<T['read']>;
+
+export const date: Type<Date> = {
+  container: 'none',
+  read: (o: UntypedJson): Date => {
+    const d = new Date(o ? o.toString() : '');
+    return isNaN(d.getTime()) ? parseError('date', o) : d;
+  },
+  write: t => t.toISOString()
+};
+
+export const str: Type<string> = {
+  container: 'none',
+  read: (o: UntypedJson): string => {
+    return typeof o == 'string' ? o : parseError('string', o);
+  },
+  write: t => t
+}
+
+export const nothing: Type<void> = {
+  container: 'none',
+  read: (o: any): void => {},
+  write: o => ''
+}
+
+export const bool: Type<boolean> = {
+  container: 'none',
+  read: (o: any): boolean => typeof o == 'boolean' ? o : parseError('boolean', o),
+  write: o => o
+}
+
+export const int: Type<number> = {
+  container: 'none',
+  read: (o: any): number => {
+    const i = parseInt(o);
+    return isNaN(i) ? parseError('integer', o) : i;
+  },
+  write: o => o
+}
+
+export const float: Type<number> = {
+  container: 'none',
+  read: (o: any): number => {
+    const i = parseFloat(o);
+    return isNaN(i) ? parseError('float', o) : i;
+  },
+  write: o => o
+}
+
+export const raw: Type<any> = {
+  container: 'none',
+  read: o => o,
+  write: o => o
+}
+
+export function optional<T>(s: Type<T>): Type<T | undefined> {
+  return {
+    container: 'none',
+    read: (o: UntypedJson): T | undefined => o == null ? undefined : s.read(o),
+    write: (t: T | undefined): UntypedJson => t == null ? null : s.write(t)
+  }
+}
+
+export function nullable<T>(s: Type<T>): Type<T | null> {
+  return {
+    container: 'none',
+    read: (o: UntypedJson): T | null => o == null ? null : s.read(o),
+    write: (t: T | null): UntypedJson => t == null ? null : s.write(t)
+  }
+}
+
+export function array<T>(s: Type<T>): List<T[]> {
+  return {
+    container: 'list',
+    read: (o: any): T[] => {
+      return o instanceof Array ? o.map(s.read) : parseError('array', o);
+    },
+    write: (o: T[]): UntypedJson => o.map(s.write)
+  }
+}
+
+export function obj<T extends Record<string, Type<any>>>(def: T)
+: Obj<{ [key in keyof T]: TypeEncapsulatedBy<T[key]> }>
+{
+  type R = { [key in keyof T]: TypeEncapsulatedBy<T[key]> };
+  return {
+    container: 'obj',
+    read: (o: UntypedJson): R => {
+      const out: any = {};
+      for (let key of Object.keys(def)) {
+        if (o instanceof Object && !(o instanceof Array)) {
+          out[key] = def[key].read(o[key]);
+        } else {
+          parseError('an object', o);
+        }
+      }
+      return out;
+    },
+    write: (r: R): UntypedJson => {
+      const out: any = {};
+      for (let key of Object.keys(def)) {
+        out[key] = def[key].write(r[key]);
+      }
+      return out;
+    }
+  }
+}
+
+export function tuple<T extends Array<SafeSerializer<any>>>(...def: T)
+  : Tuple<{ [key in keyof T]: T[key] extends SafeSerializer<any> ? TypeEncapsulatedBy<T[key]> : never }>
+{
+  type R = { [key in keyof T]: T[key] extends SafeSerializer<any> ? TypeEncapsulatedBy<T[key]> : never };
+  return {
+    container: 'tuple',
+    read: (o: UntypedJson): R => {
+      if (o instanceof Array) {
+        return def.map((d, i) => d.read(o[i])) as any;
+      } else {
+        return parseError('an array', o);
+      }
+    },
+    write: (r: R): UntypedJson => def.map((d, i) => d.write(r[i]))
+  }
+}
